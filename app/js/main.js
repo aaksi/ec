95d8373280797e0dc9f43750f38b3afce1b35860
aaksi <yyff91@mail.ru>
2021-01-08 15:26:47 +0300

    added all-course page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "14px",
+        spacing: "5px",
+        ratedFill: "#01BA57",
+      });
 
 
 });
